88db611bb495c066c487fdaf171feb77726cf0f2
CDNJ <contact.cdnjmusic@gmail.com>
2023-10-07 01:09:47 +0530

    cannot find free AI

--- bin/config.js
+++
@@ -1,5 +0,0 @@
-const API_KEY = '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';
-
-module.exports = {
-    API_KEY,
-};
--- bin/terminal-ai.js
+++ bin/terminal-ai.js
@@ -1,6 +1,9 @@
-#!/usr/bin/env node
+const openai = require('openai');
 
-const { API_KEY } = require('./config');
+// Use your own API Key to authenticate
+require('dotenv').config();
+const apiKey = process.env.OPENAI_API_KEY; // Corrected
+const client = new openai({ apiKey });
 
 console.log('Terminal-AI here!');
 
@@ -12,7 +15,35 @@ if (args.length === 0) {
 } else {
   const prompt = args.join(' ');
 
-  // Here, you can implement logic to process the prompt, communicate with your AI, and display results.
-  // For simplicity, we'll just echo the prompt for now.
-  console.log(`You asked: ${prompt}`);
+  // Display prompt in terminal
+  console.log(prompt);
+
+  // model parameterization
+  // const model = 'text-davinci-002';
+
+  // Send the prompt to OpenAI GPT-3
+  async function getGpt3Response(prompt) {
+    try {
+      const response = await client.completions.create({
+        engine: 'text-davinci-002', // Choose the engine you want
+        prompt: prompt,
+        max_tokens: 50, // Adjust the number of tokens as needed
+        model: 'davinci'
+      });
+
+      return response.choices[0].text;
+    } catch (error) {
+      console.error('Error calling OpenAI API:', error);
+      return 'An error occurred while generating a response.';
+    }
+  }
+
+  // Call the function with the user's prompt
+  getGpt3Response(prompt)
+    .then((result) => {
+      console.log('AI Response:', result);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
 }
